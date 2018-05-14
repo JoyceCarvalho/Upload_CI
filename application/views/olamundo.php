64b279de4c83741f01061ba234eecd868d940542
Joyce Carvalho <joycegcarvalho@hotmail.com>
2018-05-14 09:24:32 -0300

    Exemplo de upload de imagem com codeigniter

---
+++ application/views/olamundo.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title><?php echo $mensagem; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $mensagem; ?></h1>
+        <a href="<?php echo base_url(); ?>olamundo/teste">Primeiro teste</a>
+        <a href="<?php echo base_url(); ?>olamundo/testedb">Segundo teste</a>
+    </body>
+</html>
